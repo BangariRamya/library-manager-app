5e9cd686bd693288740903cc9b0351eec26c61ed
ramya <bangariramya444@gmail.com>
2021-12-14 19:19:37 +0530

    library-manager-app

--- src/form.jsx
+++ src/form.jsx
@@ -1,55 +1,77 @@
 import { useState } from "react";
 import { Modal, ModalBody, ModalHeader } from "reactstrap";
 
-const Form = ({add, setAdd}) => {
+const Form = ({ add, setAdd }) => {
+  const [inputs, setInputs] = useState({});
+  const [formOpen, setFormOpen] = useState(false);
 
-const [inputs,setInputs] = useState({});
-const [formOpen, setFormOpen] = useState(false);
-
-const handleInputs = (e) => {
+  const handleInputs = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    setInputs((prev) => ({...prev, [name]:value}));
-}
+    setInputs((prev) => ({ ...prev, [name]: value }));
+  };
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
     setAdd([...add, inputs]);
     //console.log(inputs);
     setInputs({});
     setFormOpen(false);
-}
+  };
 
-
-
-return(
+  return (
     <div>
-        <center><button onClick={() => setFormOpen(true)} className="form-button">Click to add new book</button></center>
+      <center>
+        <button onClick={() => setFormOpen(true)} className="form-button">
+          Click to add new book
+        </button>
+      </center>
 
-        {formOpen && <Modal isOpen modalClassName="modal">
+      {formOpen && (
+        <Modal isOpen modalClassName="modal">
+          <ModalHeader className="form-heading">Add New Book</ModalHeader>
 
-            <ModalHeader className="form-heading">Add New Book</ModalHeader>
-
-            <ModalBody>
-        
-                <form onSubmit={handleSubmit}>
-                    <label>Title :</label>
-                    <input type="text" placeholder="Enter title" name="title" value={inputs.title || ""} onChange={handleInputs}/> <br/>
-                    <label>Author :</label>
-                    <input type="text" placeholder="Enter author name" name="author" value={inputs.author || ""} onChange={handleInputs}/> <br/>
-                    <label>Subject :</label>
-                    <input type="text" placeholder="Enter subject name" name="subject" value={inputs.subject || ""} onChange={handleInputs}/> <br/> 
-
-                   {<input type="submit" disabled={!inputs.title || !inputs.author || !inputs.subject} />}
-
-                   {/* {inputs.title&&inputs.author&&inputs.subject && <input type="submit"/>} */}
-
-                </form>
-                
-            </ModalBody>
-            
-        </Modal>}
+          <ModalBody>
+            <form onSubmit={handleSubmit}>
+              <label>Title :</label>
+              <input
+                type="text"
+                placeholder="Enter title"
+                name="title"
+                value={inputs.title || ""}
+                onChange={handleInputs}
+              />{" "}
+              <br />
+              <label>Author :</label>
+              <input
+                type="text"
+                placeholder="Enter author name"
+                name="author"
+                value={inputs.author || ""}
+                onChange={handleInputs}
+              />{" "}
+              <br />
+              <label>Subject :</label>
+              <input
+                type="text"
+                placeholder="Enter subject name"
+                name="subject"
+                value={inputs.subject || ""}
+                onChange={handleInputs}
+              />{" "}
+              <br />
+              {
+                <input
+                  type="submit"
+                  disabled={!inputs.title || !inputs.author || !inputs.subject}
+                />
+              }
+              {/* {inputs.title&&inputs.author&&inputs.subject && <input type="submit"/>} */}
+            </form>
+          </ModalBody>
+        </Modal>
+      )}
     </div>
-    )
-}
+  );
+};
 export default Form;
--- src/home.jsx
+++ src/home.jsx
@@ -2,114 +2,167 @@ import { useState } from "react";
 import Form from "./form";
 
 const Home = () => {
+  const [add, setAdd] = useState([
+    {
+      title: "Differential Calculus",
+      author: "Dr.P.K.Mittal",
+      subject: "Mathematics",
+    },
+    { title: "Infinite Powers", author: "Steven Strogatz", subject: "Science" },
+    {
+      title: "You are Unique",
+      author: "A.P.J Abdul Kalam",
+      subject: "English",
+    },
+    {
+      title: "Cloud and waves",
+      author: "Rabindranath Tagore",
+      subject: "Inspirational",
+    },
+    {
+      title: "Towards Freedom",
+      author: "Jawaharlal Nehru",
+      subject: "Autobiography",
+    },
+    {
+      title: "To the Youth of India",
+      author: "Swami Vivekananda",
+      subject: "Inspirational",
+    },
+    { title: "Chaos", author: "James Gleick", subject: "Science" },
+    {
+      title: "Inspiring Thoughts",
+      author: "Mahatma Gandhi",
+      subject: "Inspirational",
+    },
+    { title: "Forbidden", author: "J.Douglas Kenyon", subject: "Science" },
+    {
+      title: "Survival Math",
+      author: "Mitchell S. Jackson",
+      subject: "Mathematics",
+    },
+    { title: "Zero to One", author: "Peter Thiel", subject: "Motivational" },
+    {
+      title: "The Math Book",
+      author: "Clifford A. Pickover",
+      subject: "Mathematics",
+    },
+  ]);
 
-    const [add, setAdd] = useState([
-                                     
-                                    {title: 'Differential Calculus', author: 'Dr.P.K.Mittal', subject: 'Mathematics'},
-                                    {title: 'Infinite Powers', author: 'Steven Strogatz', subject: 'Science'},
-                                    {title: 'You are Unique', author: 'A.P.J Abdul Kalam', subject: 'English'},
-                                    {title: 'Cloud and waves', author: 'Rabindranath Tagore', subject: 'Inspirational'},
-                                    {title: 'Towards Freedom', author: 'Jawaharlal Nehru', subject: 'Autobiography'},
-                                    {title: 'To the Youth of India', author: 'Swami Vivekananda', subject: 'Inspirational'},
-                                    {title: 'Chaos', author: 'James Gleick', subject: 'Science'},
-                                    {title: 'Inspiring Thoughts', author: 'Mahatma Gandhi', subject: 'Inspirational'},
-                                    {title: 'Forbidden', author: 'J.Douglas Kenyon', subject: 'Science'},
-                                    {title: 'Survival Math', author: 'Mitchell S. Jackson', subject: 'Mathematics'},
-                                    {title: 'Zero to One', author: 'Peter Thiel', subject: 'Motivational'},
-                                    {title: 'The Math Book', author: 'Clifford A. Pickover', subject: 'Mathematics'}
-                                    
-                                    ])
-    
-    const [editing, setEditing] = useState(null); 
-    const [editInputs, setEditInputs] = useState({});
+  const [editing, setEditing] = useState(null);
+  const [editInputs, setEditInputs] = useState({});
 
-    const handleDelete = (indexPassed) => {
-        const filterValue = add.filter((item,index) => index !== indexPassed);
-        setAdd(filterValue);
-    }                                 
+  const handleDelete = (indexPassed) => {
+    const filterValue = add.filter((item, index) => index !== indexPassed);
+    setAdd(filterValue);
+  };
 
-    
+  const handleEditInputs = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setEditInputs((prev) => ({ ...prev, [name]: value }));
+  };
 
-    const handleEditInputs = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
-        setEditInputs((prev) => ({...prev, [name]:value}));
-    } 
-    
-    const submitEdit = (indexValue) => {
-         const editedInputs = add.map((editItem,index) => {
-                                                if(index === indexValue)
-                                                {
-                                                    editItem.title = editInputs.newTitle;
-                                                    editItem.author = editInputs.newAuthor;
-                                                    editItem.subject = editInputs.newSubject;
-                                                }
-                                                return editItem;
-                                            } )
-        setAdd(editedInputs);                                 
-        setEditing(null);  
-        setEditInputs({});  
-        //console.log(editedInputs);
-    }
+  const submitEdit = (indexValue) => {
+    const editedInputs = add.map((editItem, index) => {
+      if (index === indexValue) {
+        editItem.title = editInputs.title;
+        editItem.author = editInputs.author;
+        editItem.subject = editInputs.subject;
+      }
+      return editItem;
+    });
+    setAdd(editedInputs);
+    setEditing(null);
+    setEditInputs({});
+    //console.log(editedInputs);
+  };
 
-    return(
+  return (
+    <>
+      <h1>
+        <center>Library Manager App</center>
+      </h1>
 
-        <>
-           <h1><center>Library Manager App</center></h1>  
+      <Form add={add} setAdd={setAdd} />
 
-           <Form add={add} setAdd={setAdd}/>  
-             
-          <div className="home">
+      <div className="home">
+        {add.map((item, index) => (
+          <div key={index} className="home-items">
+            <div className="home-children">
+              {editing === index ? (
+                <div className="home-child">
+                  <input
+                    type="text"
+                    name="title"
+                    placeholder="New title"
+                    value={editInputs.title || ""}
+                    onChange={handleEditInputs}
+                  />
+                  {/* {console.log(editInputs)} */}
+                  <input
+                    type="text"
+                    name="author"
+                    placeholder="New author"
+                    value={editInputs.author || ""}
+                    onChange={handleEditInputs}
+                  />
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="New subject"
+                    value={editInputs.subject || ""}
+                    onChange={handleEditInputs}
+                  />
+                </div>
+              ) : (
+                <>
+                  <p>{item.title}</p>
+                  <p>{item.author}</p>
+                  <p>{item.subject}</p>
+                </>
+              )}
+            </div>
 
-            {add.map((item,index) => (
-                              <div key={index} className="home-items">
-                                  <div className="home-children">
-                                        {editing === index 
-                                        
-                                        ?
-                                        ( 
-                                            <div className="home-child">
-                                                <input type="text" name="newTitle"  placeholder="New title" value={editInputs.newTitle || ""}  onChange={handleEditInputs}/>
-                                                <input type="text" name="newAuthor" placeholder="New author" value={editInputs.newAuthor || ""}  onChange={handleEditInputs}/>
-                                                <input type="text" name="newSubject" placeholder="New subject" value={editInputs.newSubject || ""}  onChange={handleEditInputs}/>
-                                            </div>
-                                        )     
-                                        : 
-                                        (
-                                            <>
-                                                <p>{item.title}</p> 
-                                                <p>{item.author}</p>
-                                                <p>{item.subject}</p>
-                                            </>
-                                        )
-                                      }
-                                  </div>
-
-                                  <div className="home-children">
-                                        {editing === index 
-                                        ?
-                                        (
-                                          <div className="home-children-sc">
-                                            <button onClick={() => submitEdit(index)} className="submit-edit">Submit Edits</button> &nbsp; &nbsp;  
-                                            <button onClick={() => setEditing(null)} className="cancel">Cancel</button>
-                                          </div>
-                                        )
-                                        :
-                                        (
-                                         <div className="home-children-buttons">
-                                            <button onClick={() => setEditing(index)} className="edit">Edit</button>  &nbsp; &nbsp;
-                                            <button onClick={() => handleDelete(index)} className="delete">Delete</button> 
-                                         </div>
-                                        ) 
-                                        }
-                                 </div>    
-                                 
-                    
-                              </div>
-                             ))} 
-           </div>
-           
-        </>
-    )
-}
+            <div className="home-children">
+              {editing === index ? (
+                <div className="home-children-sc">
+                  <button
+                    onClick={() => submitEdit(index)}
+                    className="submit-edit"
+                  >
+                    Submit Edits
+                  </button>
+                  &nbsp; &nbsp;
+                  <button onClick={() => setEditing(null)} className="cancel">
+                    Cancel
+                  </button>
+                </div>
+              ) : (
+                <div className="home-children-buttons">
+                  <button
+                    onClick={() => {
+                      setEditing(index);
+                      setEditInputs(item);
+                    }}
+                    className="edit"
+                  >
+                    Edit
+                  </button>
+                  &nbsp; &nbsp;
+                  <button
+                    onClick={() => handleDelete(index)}
+                    className="delete"
+                  >
+                    Delete
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 export default Home;
